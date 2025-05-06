e87e6ffadc4dc56b98749f12c3834bab14fbc825
Jonathan Miller <jdm454@cornell.edu>
2025-05-06 17:55:27 -0400

    updated levels and tilesets

---
+++ assets/tilesets/Temple L1-2 - Copy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Temple L1-2 - Copy" tilewidth="64" tileheight="64" tilecount="117" columns="13">
+ <image source="Temple L1-2 - Copy.png" width="832" height="576"/>
+</tileset>
