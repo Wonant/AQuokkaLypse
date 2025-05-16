03b1baacf9fc0daef0d1ed1a1e56f8016a1acd0c
tc572 <tc572@cornell.edu>
2025-05-16 18:31:24 -0400

    float

--- core/src/main/java/edu/cornell/cis3152/physics/platform/DreamDweller.java
+++ core/src/main/java/edu/cornell/cis3152/physics/platform/DreamDweller.java
@@ -193,7 +193,10 @@ public class DreamDweller extends Enemy {
     public void update(float dt) {
         lookForPlayer();
 
-        // 设置 sprite 朝向
+        if (obstacle != null && obstacle.getBody() != null) {
+            obstacle.getBody().setGravityScale(0);
+        }
+
         Vector2 playerPos = scene.getAvatar().getObstacle().getPosition();
         Vector2 selfPos = obstacle.getPosition();
         facingRight = playerPos.x >= selfPos.x;
